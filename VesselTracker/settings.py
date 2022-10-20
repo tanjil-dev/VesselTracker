27c8168195c3b59d0a5fc39473f14c9afbf19428
Tanjil Ahmed Ovi <tanzil.ovi578@gmail.com>
2022-10-20 21:51:34 +0900

    hosts

--- VesselTracker/settings.py
+++ VesselTracker/settings.py
@@ -9,7 +9,7 @@ SECRET_KEY = 'django-insecure--q&w4pj80p0g9zlk(ao_+79xe9@4m1cm^y=p=av(zg3hpp!_cm
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["0.0.0.0", "35.76.113.93"]
 
 
 INSTALLED_APPS = [
