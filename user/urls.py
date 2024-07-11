501ed2e3e7b5edeb73d13af9808d9ba3f98ce983
Tanjil Ahmed Ovi <tanzil.ovi578@gmail.com>
2024-07-11 17:46:29 +0600

    search

--- user/urls.py
+++ user/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from user.views import *
 
 urlpatterns = [
-    path('', Home, name='home'),
-    path('vessel/', VesselView, name='vessel'),
+    path('', Home.as_view(), name='home'),
+    path('vessel/', VesselView.as_view(), name='vessel'),
     path('login/', Login, name='login'),
     path('signup/', Signup, name='signup'),
     path('logout/', LogoutUser, name='logout'),
