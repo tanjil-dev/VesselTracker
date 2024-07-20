b976f66694b4a9b91b7e46ed020069ca1ab03eee
Tanjil Ahmed Ovi <tanzil.ovi578@gmail.com>
2024-07-20 15:24:04 +0600

    parcel api and parcel model bugfix

--- user/models.py
+++ user/models.py
@@ -11,13 +11,11 @@ GENDER_CHOICES = (
 
 class Parcel(models.Model):
     transaction_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
-    voyage = models.ForeignKey(Voyage, on_delete=models.CASCADE, related_name='parcels')
+    voyage = models.ForeignKey(Voyage, on_delete=models.CASCADE)
     description = models.TextField(blank=True, null=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     received = models.BooleanField(default=False)
 
-    def __str__(self):
-        return self.transaction_id
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
