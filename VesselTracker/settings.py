85b820e91f50e1e5c24aa2481dfcfc7fb49a550e
Tanjil Ahmed Ovi <tanzil.ovi578@gmail.com>
2024-07-30 23:42:29 +0600

    password reset feature

--- VesselTracker/settings.py
+++ VesselTracker/settings.py
@@ -133,3 +133,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
+
+
+#email
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'tanzil.ovi578@gmail.com'
+EMAIL_HOST_PASSWORD = 'uacmyugnsnqtmudq'
