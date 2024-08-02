39a72b015b6866a6eafadaf0f21ab73fa2e049ba
Tanjil Ahmed Ovi <tanzil.ovi578@gmail.com>
2024-08-02 15:53:43 +0600

    [KAN-5] vercel 1

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,11 @@
-echo "BUILD START"
+#!/bin/bash
+
+# Install dependencies
 pip install -r requirements.txt
-python3.6 manage.py collectstatic --noinput --clear
-echo "BUILD END"
+
+# Collect static files
+python manage.py collectstatic --noinput
+
+# Ensure the output directory exists
+mkdir -p staticfiles_build
+mv static staticfiles_build/
